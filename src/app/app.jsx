2d775adc25cfe96ae5201618956a7dce34cd38f5
nanoix9 <nanoix9@gmail.com>
2019-08-16 12:51:05 +1200

    add react

---
+++ src/app/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function element() {
+  return (
+    <h1>Hello, world!</h1>
+  );
+}
+
+export default element;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const path = require('path');
+
 const app = express()
 const port = 3000
 
-app.set('views', __dirname + '/views');
+const res_path = path.join(path.dirname(__dirname), 'dist', 'frontend');
+
+app.set('views', res_path);
 app.engine('html', require('ejs').renderFile);
+app.use(express.static(res_path));
 
 app.get('/hello', (req, res) => res.send('Hello World!'))
 
 app.get('/', (req, res) => {
-    res.render('index.html');
+  res.render('index.html');
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
